aa0d2e5f42e5f92f4a1a00498e1672c33d7de7bd
Marcos Paulo Ignacio <94194033+Marcos-Ignacio@users.noreply.github.com>
2022-03-24 21:05:14 -0300

    Feat:component Footer (#7)

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css"
+
+function Footer (props) {
+    return (
+        <div className="footer">
+            <p>São Paulo, SP - 89251-000 | Mee Company, all rights reserved.</p>
+        <div className="redes">
+            <img src="img/instagram.svg" alt="" />
+            <img src="img/Vector.svg" alt=""/>
+        </div>
+        </div>
+    )
+}
+
+export default Footer
