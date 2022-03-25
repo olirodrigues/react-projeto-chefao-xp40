e94ce6436f0567d6acaadee726775e0002340c2b
Leonardo Paixão <leox95@hotmail.com>
2022-03-25 15:43:10 -0300

    uso do componente texto em header e contato

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom'
 import './estiloheader.css'
 import LogoMee from '../../assets/logoMee.svg'
+import Texto from '../Texto/Texto'
 
 
 
@@ -8,10 +9,18 @@ function Header(){
     return(
         <nav>
             <img className='imgLogo' src={LogoMee} alt="Logo da empresa com nome MEE"/>
-            <Link className="headerLink" to="/plantas">Plantas</Link>
-            <Link className="headerLink" to="/sobrenos">Sobre nós</Link>
-            <Link className="headerLink" to="/blog">Blog</Link>
-            <Link className="headerLink" to="/contato">Contato</Link>
+            <Link className='headerLink' to="/plantas">
+            <Texto tipo='paragrafo' children='Plantas'/>
+            </Link>
+            <Link className='headerLink' to="/sobrenos">
+            <Texto tipo='paragrafo' children='Sobre nós' />
+            </Link>
+            <Link className='headerLink' to="/blog">
+            <Texto tipo='paragrafo' children='Blog'/>
+            </Link>
+            <Link className='headerLink' to="/contato">
+            <Texto tipo='paragrafo' children='Contato'/>
+            </Link>
         </nav>
     )
 }
--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,16 +1,19 @@
 
 import Form from '../../components/Form'
 import './estiloContato.css'
+import Texto from '../../components/Texto/Texto';
 
 function Contato() {
-    return (
-      <div >
-        <article className='textoContato'>
-        <p>Seria ótimo se nossas plantinhas falassem. Mas nós podemos aprender e entender todos os sinais elas que nos dão. Mande sua dúvida que teremos prazer em responder.</p>
-        </article>
-        <Form/>
-      </div>
-    );
-  }
-  
-  export default Contato;
+  return (
+    <div >
+      <article className='textoContato'>
+        <Texto tipo='titulo1'
+          children='Seria ótimo se nossas plantinhas falassem. Mas nós podemos aprender e entender todos os sinais elas que nos dão. Mande sua dúvida que teremos prazer em responder.'
+          class='h1-contato' />
+      </article>
+      <Form />
+    </div>
+  );
+}
+
+export default Contato;
