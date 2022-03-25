89f9810b143a58252c534bfad20bd37090f6da15
Leonardo Paixão <leox95@hotmail.com>
2022-03-25 14:42:53 -0300

    icones realizados

---
+++ src/components/IconePata/index.js
@@ -0,0 +1,12 @@
+import './estiloIconePata.css'
+import Pata from './imgPata.png'
+
+
+function IconePata(){
+    return(
+    <div className='fundoIconePata'>
+        <img src={Pata} className='iconePata'/>
+    </div>
+    )
+}
+export default IconePata
