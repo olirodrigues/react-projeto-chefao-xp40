595e53bdced97c9a2d56c68c6a1ad92073513ecb
Leonardo Paixão <leox95@hotmail.com>
2022-03-31 15:11:16 -0300

    todas as responsividades em um mesmo componente

--- src/components/GridImgBlogDesktop/index.js
+++ src/components/GridImgBlogDesktop/index.js
@@ -5,7 +5,7 @@ function GridImgBlogDesktop(props){
     return(
     <div className='container-grid'>
         <div className='item-grid h-1'>
-            <img src={props.src1} className='img-grid' alt='imagem do blog de plantas'/>
+            <img src={props.src1} className='img-grid i-1' alt='imagem do blog de plantas'/>
         </div>
         <div className='item-grid h-2'>
             <img src={props.src2} className='img-grid' alt='imagem do blog de plantas'/>
--- src/pages/Blog/index.js
+++ src/pages/Blog/index.js
@@ -1,12 +1,13 @@
-import GridImgBlogTablet from "../../components/GridImgBlogTablet";
+import GridImgBlogDesktop from "../../components/GridImgBlogDesktop";
 
 
 function Blog() {
   return (
       <div>
-        <GridImgBlogTablet src1="https://gabezrodz.github.io/imagens-chefao-xp40/blog/BD0101.png"
+        <GridImgBlogDesktop src1="https://gabezrodz.github.io/imagens-chefao-xp40/blog/BD0101.png"
         src2="https://gabezrodz.github.io/imagens-chefao-xp40/blog/BD0101.png"
-        src3="https://gabezrodz.github.io/imagens-chefao-xp40/blog/BD0101.png"/>
+        src3="https://gabezrodz.github.io/imagens-chefao-xp40/blog/BD0101.png"
+        src4="https://gabezrodz.github.io/imagens-chefao-xp40/blog/BD0101.png"/>
         </div>
   );
 }
