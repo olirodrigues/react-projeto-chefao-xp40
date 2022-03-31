9526bdbdeafd5bfb096e22c7854e78761fabbae5
olirodrigues <olivia.rt@gmail.com>
2022-03-31 10:32:56 -0300

    Feat: Pagina Planta Detalhe

---
+++ src/pages/PlantasDetalhe/index.js
@@ -0,0 +1,14 @@
+import "./PlantasDetalhe.css";
+import Abas from "../../components/Abas/Abas";
+import Carrossel from "../../components/Carrossel/Carrossel";
+
+function PlantasDetalhe() {
+  return (
+    <div id="main-content">
+      <Abas />
+      <Carrossel />
+    </div>
+  );
+}
+
+export default PlantasDetalhe;
