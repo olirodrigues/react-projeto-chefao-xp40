615e31192855a1889aea3f55ec0ae4dcf1e8cc19
Marcos Ignacio <marcos.st.manu@gmail.com>
2022-03-31 22:26:44 -0300

    Bugfix: Usando o componente texte no footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,16 @@
 import "./Footer.css";
+import Texto from "../Texto/Texto";
 
 function Footer(props) {
   return (
     <footer>
-      <div className='footer'>
-        <p>São Paulo, SP - 89251-000 | Mee Company, all rights reserved.</p>
-        <div className='redes'>
-          <img src='img/instagram.svg' alt='' />
-          <img src='img/Vector.svg' alt='' />
+      <div className="footer">
+        <Texto tipo="paragrafo" class="cinza-claro">
+          São Paulo, SP - 89251-000 | Mee Company, all rights reserved.
+        </Texto>
+        <div className="redes">
+          <img src="img/instagram.svg" alt="" />
+          <img src="img/Vector.svg" alt="" />
         </div>
       </div>
     </footer>
