8cb3b66c09b7eb2e913dddabe3665eda0b3c1e38
Gabriel Rodrigues <cgabrielrr@gmail.com>
2022-04-03 16:07:41 -0300

    bugfix: adicionado segunda rota para api

--- src/services/api/apiConfig.js
+++ src/services/api/apiConfig.js
@@ -1,8 +1,11 @@
 import Axios from "axios";
 import { TOKEN_API_MEE } from "../configs/keyApiMee";
 
+const url =
+  "https://api-chefao.herokuapp.com/" || "https://api-mee.herokuapp.com";
+
 export const apiMee = Axios.create({
-  baseURL: "https://api-mee.herokuapp.com",
+  baseURL: url,
   headers: {
     Authorization: TOKEN_API_MEE,
   },
