8a3c73bfc7918fef3b5f9832b218e8e7c947bd5e
olirodrigues <olivia.rt@gmail.com>
2022-04-04 21:12:03 -0300

    Hotfix: icones componente Cuidados

--- src/components/Cuidados/Cuidados.js
+++ src/components/Cuidados/Cuidados.js
@@ -1,22 +1,27 @@
 import "./Cuidados.css";
 import Texto from "../Texto/Texto";
-import IconeSol from "../IconeSol";
-import IconePata from "../IconePata";
-import IconeGota from "../IconeGota";
+import IconeSol from "../../assets/svg/plantasDetalhe/plantasSol.svg";
+import IconePet from "../../assets/svg/plantasDetalhe/plantasPet.svg";
+import IconePetToxico from "../../assets/svg/plantasDetalhe/plantasPetToxico.svg";
+import IconeAgua from "../../assets/svg/plantasDetalhe/plantasAgua.svg";
 
 function Cuidados({ iluminacao, agua, pet }) {
   return (
     <div className="container-cuidados">
       <div className="texto-cuidados">
-        <IconeSol />
+        <img src={IconeSol} alt="" />
         <Texto tipo="paragrafo">{iluminacao}</Texto>
       </div>
       <div className="texto-cuidados">
-        <IconeGota />
+        <img src={IconeAgua} alt="" />
         <Texto tipo="paragrafo">{agua}</Texto>
       </div>
       <div className="texto-cuidados">
-        <IconePata />
+        {pet === "Atenção: tóxico!" ? (
+          <img src={IconePetToxico} alt="" />
+        ) : (
+          <img src={IconePet} alt="" />
+        )}
         <Texto tipo="paragrafo">{pet}</Texto>
       </div>
     </div>
