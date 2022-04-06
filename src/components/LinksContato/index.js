1a07bc0b04ba5338603fe1bdc6ca47aba3b8635e
Leonardo Paixão <leox95@hotmail.com>
2022-04-06 16:49:07 -0300

    alterações css na página contato (#55)

--- src/components/LinksContato/index.js
+++ src/components/LinksContato/index.js
@@ -4,9 +4,15 @@ import {FaInstagram, FaRegEnvelope, FaPinterestP} from 'react-icons/fa'
 function LinksContato(){
     return(
     <div className='fundoLinkContato'>
+        <div className='circulo'>
         <FaInstagram className='linkContato'/>
+        </div>
+        <div className='circulo'>
         <FaPinterestP className='linkContato'/>
+        </div>
+        <div className='circulo'>
         <FaRegEnvelope className='linkContato'/>
+        </div>
     </div>
     )
 }
