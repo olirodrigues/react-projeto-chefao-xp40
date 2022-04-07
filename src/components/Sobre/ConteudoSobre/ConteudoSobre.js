448c3f298b17dc2a4e61eee1cace38b862344236
olirodrigues <olivia.rt@gmail.com>
2022-04-06 21:40:17 -0300

    Hotfix: Refatorando CSS Pagina SobreNos

--- src/components/Sobre/ConteudoSobre/ConteudoSobre.js
+++ src/components/Sobre/ConteudoSobre/ConteudoSobre.js
@@ -2,34 +2,30 @@ import "./ConteudoSobre.css";
 import Texto from "../../Texto/Texto";
 import { NavLink } from "react-router-dom";
 import TextosSobre from "../TextosSobre/TextosSobre";
-import GridPosts from "../../Grid/GridPosts/GridPosts";
 
-const ConteudoSobre = (props) => {
+const ConteudoSobre = ({ id }) => {
   return (
-    <div className="container-conteudo">
-      <div className="container-textos">
-        <div className="container-abas__sobre">
-          <NavLink
-            to="/sobrenos/1"
-            className={({ isActive }) => (isActive ? "aba-ativa" : "")}
-          >
-            <Texto tipo="titulo2" class="cinza">
-              Nós
-            </Texto>
-          </NavLink>
-          <NavLink
-            to="/sobrenos/2"
-            className={({ isActive }) => (isActive ? "aba-ativa" : "")}
-          >
-            <Texto tipo="titulo2" class="cinza">
-              Margarete Mee
-            </Texto>
-          </NavLink>
-        </div>
-        <TextosSobre id={props.id} />
+    <div className="container-conteudo-sobrenos">
+      <div className="container-abas__sobre">
+        <NavLink
+          to="/sobrenos/1"
+          className={({ isActive }) => (isActive ? "aba-ativa" : "nao-ativa")}
+        >
+          <Texto tipo="titulo2" class="cinza">
+            Nós
+          </Texto>
+        </NavLink>
+        <NavLink
+          to="/sobrenos/2"
+          className={({ isActive }) => (isActive ? "aba-ativa" : "")}
+        >
+          <Texto tipo="titulo2" class="cinza">
+            Margarete Mee
+          </Texto>
+        </NavLink>
       </div>
-      <div className={props.id === "1" ? "grid-sobre__nos" : "grid-sobre__mee"}>
-        < GridPosts fotos={props.fotos} urlFotos={props.urlFotos} />
+      <div className="container-texto-sobrenos">
+        <TextosSobre id={id} />
       </div>
     </div>
   );
