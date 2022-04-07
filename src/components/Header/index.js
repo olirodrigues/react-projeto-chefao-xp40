6854591f14bec2f592aa132893a06f2a5d0c57e3
Gabriel Rodrigues <cgabrielrr@gmail.com>
2022-04-06 21:51:58 -0300

    hotfix: ajustes no estilo do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,64 @@
-
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./estiloheader.css";
 import LogoMee from "../../assets/svg/logoMee/logoMee.svg";
+import Texto from "../Texto/Texto";
 
 function Header() {
   return (
     <header>
       <nav>
-        <img className='imgLogo' src={LogoMee} alt='Logo da empresa com nome MEE' />
-        <Link className='headerLink' to='/plantas'>
-          Plantas
-        </Link>
-        <Link className='headerLink' to='/sobrenos/1'>
-          Sobre nós
-        </Link>
-        <Link className='headerLink' to='/blog'>
-          Blog
-        </Link>
-        <Link className='headerLink' to='/contato'>
-          Contato
-        </Link>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "link_ativo--logo" : "link_inativo--logo"
+          }
+        >
+          <img
+            className="imgLogo"
+            src={LogoMee}
+            alt="Logo da empresa com nome MEE"
+          />
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "link_ativo-desktop headerLink" : " link_inativo headerLink "
+          }
+          to="/plantas"
+        >
+          <Texto tipo="paragrafo" class="desktop-menu">
+            Plantas
+          </Texto>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "link_ativo-desktop headerLink" : " link_inativo headerLink"
+          }
+          to="/sobrenos"
+        >
+          <Texto tipo="paragrafo" class="desktop-menu">
+            Sobrenos
+          </Texto>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "link_ativo-desktop headerLink" : " link_inativo headerLink"
+          }
+          to="/blog"
+        >
+          <Texto tipo="paragrafo" class="desktop-menu">
+            Blog
+          </Texto>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "link_ativo-desktop headerLink" : " link_inativo headerLink"
+          }
+          to="/contato"
+        >
+          <Texto tipo="paragrafo" class="desktop-menu">
+            Contato
+          </Texto>
+        </NavLink>
       </nav>
     </header>
   );
