f3b56c94126cb2b9e88a660975a079875d17ca98
Gabriel Rodrigues <cgabrielrr@gmail.com>
2022-04-06 23:09:20 -0300

    Hotfix: adc link ao card do blog

--- src/components/Blog/ListaDePosts/BlogListaPosts.js
+++ src/components/Blog/ListaDePosts/BlogListaPosts.js
@@ -6,33 +6,35 @@ import { Link } from "react-router-dom";
 
 const BlogListaPosts = (props) => {
   return (
-    <div className="card-container">
-      <div className="card-container__imagem">
-        <img
-          src={`https://gabezrodz.github.io/imagens-chefao-xp40/blog/${props.fotos.Capa_do_blog}.png`}
-          alt=""
-        />
-      </div>
-      <div className="card-textos">
-        <div className="titulo-post">
-          <Texto tipo="titulo2" class="cinza-escuro h2-blog">
-            {props.titulo}
-          </Texto>
+    <Link to={`/blog/${props.id}`}>
+      <div className="card-container">
+        <div className="card-container__imagem">
+          <img
+            src={`https://gabezrodz.github.io/imagens-chefao-xp40/blog/${props.fotos.Capa_do_blog}.png`}
+            alt=""
+          />
+        </div>
+        <div className="card-textos">
+          <div className="titulo-post">
+            <Texto tipo="titulo2" class="cinza-escuro h2-blog">
+              {props.titulo}
+            </Texto>
+          </div>
+          <div>
+            <Texto tipo="paragrafo" class="cinza-escuro">
+              {props.resumo}
+            </Texto>
+          </div>
         </div>
         <div>
-          <Texto tipo="paragrafo" class="cinza-escuro">
-            {props.resumo}
-          </Texto>
+          <Link to={`/blog/${props.id}`}>
+            <Botao class="outline" color="cinza bold">
+              Ler artigo
+            </Botao>
+          </Link>
         </div>
       </div>
-      <div>
-        <Link to={`/blog/${props.id}`}>
-          <Botao class="outline" color="cinza bold">
-            Ler artigo
-          </Botao>
-        </Link>
-      </div>
-    </div>
+    </Link>
   );
 };
 
