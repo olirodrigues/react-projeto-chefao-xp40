a80e7ae414adc8f9b967f3cce0fa6d935af11233
olirodrigues <olivia.rt@gmail.com>
2022-04-08 01:36:44 -0300

    Hotfix: Luminosidade planta detalhe

--- src/pages/PlantasDetalhe/PlantasDetalhe.js
+++ src/pages/PlantasDetalhe/PlantasDetalhe.js
@@ -10,7 +10,6 @@ import { Link, useParams } from "react-router-dom";
 import { getPlantasId } from "../../services/routes/plantas";
 import AnimacaoLoading from "../../components/AnimacaoLoading/AnimacaoLoading";
 
-
 function PlantasDetalhe() {
   const [plantas, setPlantas] = useState(null);
   const params = useParams();
@@ -24,6 +23,7 @@ function PlantasDetalhe() {
       nome_cientifico: response.informaco.nome_cientifico,
       categoria_descricao: response.informaco.categoria_descricao,
       altura: response.informaco.altura,
+      luminosidade: response.informaco.luminosidade,
       cuidados_iluminacao: response.cuidados_iluminacao,
       cuidados_agua: response.cuidados_agua,
       cuidados_pet: response.cuidados_pet,
@@ -74,6 +74,12 @@ function PlantasDetalhe() {
           Altura:{" "}
         </Texto>
         <Texto tipo="paragrafo">{plantas.altura}</Texto>
+      </div>
+      <div>
+        <Texto tipo="paragrafo" class="bold">
+          Luminosidade:{" "}
+        </Texto>
+        <Texto tipo="paragrafo">{plantas.luminosidade}</Texto>
       </div>
     </div>
   );
