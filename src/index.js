7e4fcfd63fd72cc48e392cdace27b6a205392b76
olirodrigues <olivia.rt@gmail.com>
2022-04-08 10:24:21 -0300

    Feat: Configuração PWA

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
 
-
+serviceWorkerRegistration.register();
+reportWebVitals();
