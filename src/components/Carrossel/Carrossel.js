94e9e858831b06027d97e3fcb2a75502bf820962
olirodrigues <olivia.rt@gmail.com>
2022-04-08 01:18:47 -0300

    Hotfix: Setas do carrossel funcionais

--- src/components/Carrossel/Carrossel.js
+++ src/components/Carrossel/Carrossel.js
@@ -18,16 +18,15 @@ function Carrossel(props) {
     }, {});
 
   const [fotoCarrossel, setFotoCarrossel] = useState(0);
-  const [current, setCurrent] = useState(0);
   const lenght = Object.entries(fotosSizes).length;
 
-  // const nextSlide = () => {
-  //   setCurrent(current === 0 ? lenght - 1 : current - 1);
-  // };
+  const nextSlide = () => {
+    setFotoCarrossel(fotoCarrossel === 0 ? lenght - 1 : fotoCarrossel - 1);
+  };
 
-  // const plevSlide = () => {
-  //   setCurrent(current === lenght - 1 ? 0 : current + 1);
-  // };
+  const plevSlide = () => {
+    setFotoCarrossel(fotoCarrossel === lenght - 1 ? 0 : fotoCarrossel + 1);
+  };
 
   const animacaoCarrossel = (index) => {
     if (fotoCarrossel === 0) {
@@ -61,47 +60,48 @@ function Carrossel(props) {
 
   return (
     <div className="container-carrossel-grid">
-      <div className="container-carrossel">
+      <div className="container-carrossel-setas">
+        <div className="container-carrossel">
+          {Object.entries(fotosSizes).map(([fotos, sizes], index) => {
+            return (
+              <ImagemFolha
+                key={fotos}
+                src={fotos}
+                sizes={sizes}
+                className={`sem-borda ${animacaoCarrossel(index)}`}
+                maxMobileSize="250px"
+                maxTabletSize="500px"
+                maxDesktopSize="500px"
+                urlImagens={`https://gabezrodz.github.io/imagens-chefao-xp40/plantas/categorias/${props.categoria}`}
+              />
+            );
+          })}
+        </div>
+
+        <div className="containter-setas__carrosel">
+          <img src={SetaEsquerda} onClick={nextSlide} alt="seta direita" />
+          <img src={SetaDireita} onClick={plevSlide} alt="seta direita" />
+        </div>
+      </div>
+
+      <div className="container-grid">
         {Object.entries(fotosSizes).map(([fotos, sizes], index) => {
           return (
             <ImagemFolha
               key={fotos}
               src={fotos}
               sizes={sizes}
-              className={`sem-borda ${animacaoCarrossel(index)}`}
-              maxMobileSize="250px"
-              maxTabletSize="500px"
-              maxDesktopSize="500px"
+              className={`sem-borda ${animacaoCarrossel(index)} ${
+                fotoCarrossel === index
+                  ? "foto-carrossel-com-foco"
+                  : "foto-carrossel-sem-foco"
+              }`}
+              maxMobileSize="30px"
+              maxTabletSize="80px"
+              maxDesktopSize="80px"
+              onClick={() => setFotoCarrossel(index)}
               urlImagens={`https://gabezrodz.github.io/imagens-chefao-xp40/plantas/categorias/${props.categoria}`}
             />
-          );
-        })}
-      </div>
-
-      <div className="container-grid">
-        {Object.entries(fotosSizes).map(([fotos, sizes], index) => {
-          return (
-            <>
-              <ImagemFolha
-                key={fotos}
-                src={fotos}
-                sizes={sizes}
-                className={`sem-borda ${animacaoCarrossel(index)} ${
-                  fotoCarrossel === index
-                    ? "foto-carrossel-com-foco"
-                    : "foto-carrossel-sem-foco"
-                }`}
-                maxMobileSize="30px"
-                maxTabletSize="80px"
-                maxDesktopSize="80px"
-                onClick={() => setFotoCarrossel(index)}
-                urlImagens={`https://gabezrodz.github.io/imagens-chefao-xp40/plantas/categorias/${props.categoria}`}
-              />
-              <div className="containter-setas__carrosel">
-                <img src={SetaEsquerda} alt="seta direita"  />
-                <img src={SetaDireita} alt="seta direita"  />
-              </div>
-            </>
           );
         })}
       </div>
